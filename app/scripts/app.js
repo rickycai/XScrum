34f7f4dcdf15611f2c9b1c38b259e17fc6807297
craigliu <chong8086@hotmail.com>
2014-04-06 10:33:10 +0800

    Adapt to requirejs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,18 @@
-'use strict';
+define([
+    'angular',
+    'angular-route',
+    './controllers/index',
+    './directives/index',
+    './filters/index',
+    './services/index'
+], function (angular) {
+    'use strict';
 
-angular.module('xscrumApp', []);
+    return angular.module('XScrum', [
+        'app.controllers',
+        'app.directives',
+        'app.filters',
+        'app.services',
+        'ngRoute'
+    ]);
+});
